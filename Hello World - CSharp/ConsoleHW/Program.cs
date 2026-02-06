73b03c36e404ce835d23ab9da2830392ee88b133
Victor Piovezana <piovezanavictor@gmail.com>
2026-02-06 00:53:45 -0300

    Added to the "Hello World" folder

---
+++ Hello World - CSharp/ConsoleHW/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
