34a82d9ab64204febf99efdc8ec9826cb0f8151a
Victor Piovezana <piovezanavictor@gmail.com>
2026-02-10 17:57:12 -0300

    Add Conversão de Tipos
    ° Melhoria na raiz das aulas
    ° Adicionado pasta Aula 07
    ° Estudos Conversões de Tipos

---
+++ A07 - Conversoes de tipos/M01A07/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace M01A07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Conversão Implícita
+            
+            int a = 7;
+            float b = a;
+            
+            Console.WriteLine("O valor de A é: " + a + " Seu tipo primitivo é: " +a.GetType());
+            Console.WriteLine("O Valor de B é: " + b + " Seu tipo primitivo é: " +b.GetType());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ A07 - Conversoes de tipos/M01A07b/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01A07b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
