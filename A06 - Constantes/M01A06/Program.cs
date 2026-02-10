390473061f67b6e345d8a296f809f82293840f4f
Victor Piovezana <piovezanavictor@gmail.com>
2026-02-10 16:02:35 -0300

    Constante em C#

---
+++ A06 - Constantes/M01A06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace M01A06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string nome = "Victor";
+            var pi = Math.PI;
+            float PI = 3.1415F;
+
+            Console.WriteLine("Eu me chamo " + nome); //Usando o comando Const a vatiavel "nunca" poderá ser trocada
+            Console.ReadKey();
+            Console.Clear();
+            
+            Console.WriteLine("O valor de pi na biblioteca Math é: " + pi); //podemos usar neste formato ou
+            Console.WriteLine("O valor de pi é: " + Math.PI); // ambos os jeitos estão corretos, porem eles usam o dobro de memória
+                                                              //pois é um comando extenso, caso queira apenas o valor, podemos usar ele como float
+            Console.WriteLine("O valor de pi em float é: " + PI); //Valor de pi em FLOAT
+            
+            Console.ReadKey();
+        }
+    }
+}
