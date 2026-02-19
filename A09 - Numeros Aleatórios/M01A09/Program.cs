86b4f99ba5e10eb06a332797042826fa20d26c22
Victor Piovezana <piovezanavictor@gmail.com>
2026-02-19 17:49:20 -0300

    Acressimo A9 ao A12
    Adição...
    
    Aula 09 - Numeros Aleatórios
    Aula 10 - Data e Hora
    Aula 11 - Termporizador de eventos
    Aula 12 - Interpolação de Strings

---
+++ A09 - Numeros Aleatórios/M01A09/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01A09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Quando gerado numeros, ele sempre perde uma unidade, limitando em 10 unidade ele gera do 0 ao 9.
+            
+
+            Random aleatorio = new Random();
+
+            int numero = aleatorio.Next(); // Ele gera números sem limitação
+
+            int numero2 = aleatorio.Next(100); // Ele vai se limitar aos números de 0 até 99
+
+            int numero3 = aleatorio.Next(10 , 21); //Ele vai gerar numeros entre 10 até 20
+
+            Console.WriteLine($"O número aleatório SEM Limite gerado é: {numero}\n");
+
+            Console.WriteLine($"O número aleatório COM Limite gerado é: {numero2}\n");
+
+            Console.WriteLine($"O número aleatório Limitado gerado é: {numero3}");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ A12 - Interpolação de Strings/M01A12/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace M01A12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome;
+            int idade;
+            Random maquina = new Random();
+            int aleatorio;
+            int aleatorioMaquina = maquina.Next(0 , 101);
+
+            Console.Write("Escreva seu nome: ");
+            nome = Console.ReadLine();
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.Write($"{nome}, agora digite sua idade: ");
+            int.TryParse(Console.ReadLine(), out idade);
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.WriteLine($"Perfeito {nome}...\nVerifiquei que você possuí: {idade} anos.\n" +
+                $"Para finalizar, preciso que você digite um número de 0 a 100, para testar seu teste de precisão!\nAguarde...");
+            Thread.Sleep(10000);
+            Console.Clear();
+
+            Console.Write("Escreva um valor entre 0 à 100...\n");
+            Console.Write("Digite: ");
+            int.TryParse(Console.ReadLine(), out aleatorio);
+            Console.Clear();
+
+            Console.WriteLine($"O número digitado é: {aleatorio} ");
+            Thread.Sleep(1000);
+            Console.WriteLine("P E N S A N D O . . .");
+            Thread.Sleep(5000);
+            Console.WriteLine($"O número em que a máquina pensou é: {aleatorioMaquina}");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("RESUMO");
+
+            Console.WriteLine($"Você se chama: {nome}.\nSua idade é: {idade}.\nO numero escolhido foi: {aleatorio}.\nNumero escolhido pela máquina: {aleatorioMaquina}.\n");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Fim do programa");
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
